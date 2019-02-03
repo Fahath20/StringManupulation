9ee1d923751cdd68def47401bd9931c5b898573f
User <user@Fahath-Mac.local>
2019-02-02 19:59:30 -0600

    Initial Commit

---
+++ StringManupulation/StringManupulation.h
@@ -0,0 +1,19 @@
+//
+//  StringManupulation.h
+//  StringManupulation
+//
+//  Created by User on 2/2/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringManupulation.
+FOUNDATION_EXPORT double StringManupulationVersionNumber;
+
+//! Project version string for StringManupulation.
+FOUNDATION_EXPORT const unsigned char StringManupulationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringManupulation/PublicHeader.h>
+
+
